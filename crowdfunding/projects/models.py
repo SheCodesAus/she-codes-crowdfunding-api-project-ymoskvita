d7df249e69a99b8696d5fda165d5dcdb2a6f8a9d
Julia Moskvita <ymoskvita@gmail.com>
2023-01-21 09:45:17 +1100

    add User model and Authentication

--- crowdfunding/projects/models.py
+++ crowdfunding/projects/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 # Create your models here.
@@ -10,8 +11,11 @@ class Project(models.Model):
     image = models.URLField()
     is_open = models.BooleanField()
     date_created = models.DateTimeField(auto_now_add=True)
-    owner = models.CharField(max_length=200)
-    # owner = models.ForeignKey()
+    owner = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='owner_projects'
+    )
 
 
 class Pledge(models.Model):
@@ -23,4 +27,8 @@ class Pledge(models.Model):
         on_delete=models.CASCADE,
         related_name='pledges'
     )
-    supporter = models.CharField(max_length=200)
+    supporter = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name='supporter_pledges'
+    )
